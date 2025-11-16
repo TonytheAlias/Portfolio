d15d33f75b270bd5fbbefa2d7884982d274c2733
Tyreek Brasfield <tyreekbrasfield@gmail.com>
2025-11-15 23:10:38 -0600

    Added project images and icons to images folder and filled in image and icon headers in projectdata.js file

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -4,8 +4,8 @@ const projectsData = [
     title: "Otus Finance",
     description: "This project is a financial app that tracks user spending and allows them to set monthly cap based on the money they receive and spend every month. It also shows the user an overview of their spending habits.",
     technologies: ["C#", ".Net Maui", "Visual Studio", "SQL Lite"],
-    image: "/project1.png",
-    icon: '',
+    image: "./images/OtusFinance.png",
+    icon: "./images/OtusFinance-icon.png",
     githubLink: "https://github.com/TonytheAlias/OtusFinance",
     feature: true
   },
@@ -14,8 +14,8 @@ const projectsData = [
     title: "Recuritment Buddy",
     description: "A web-based degree program recommender using personality traits and academic interests",
     technologies: ["HTML", "CSS", "JavaScript", "Python","VSCode", "Flask"],
-    image: "/project1.png", 
-    icon: '',
+    image: "./images/RecruitmentBuddy.png", 
+    icon: "./images/RecruitmentBuddy-icon.png",
     githubLink: "https://github.com/TonytheAlias/RecruitmentBuddy",
     feature: true
   },
@@ -24,8 +24,8 @@ const projectsData = [
     title: "Code Tracker",
     description: "A Chrome extension that monitors and analyzes user behavior during coding assessments on platforms like LeetCode and HackerRank. Tracks keystrokes, paste events, tab switches, and idle periods with real-time analytics dashboard and exports data in Excel, CSV, and PDF formats. Built with vanilla JavaScript and Chrome Extension APIs (Manifest V3)",
     technologies: ["JavaScript", "HTML", "CSS", "VSCode","Json","Manifest V3", "Chrome Stroage API"],
-    image: "/project1.png",
-    icon: "",
+    image: "./images/CodeTraker.png",
+    icon: "./images/CodeTracker-Icon.png",
     githubLink: "https://github.com/TonytheAlias/CodeTracker",
     feature: true
   },
@@ -34,8 +34,8 @@ const projectsData = [
     title: "Otaku Search",
     description: "A search engine that allow the user to search for their favorite anime, manhwa, and manga",
     technologies: ["HTML", "CSS", "JavaScript", "Python","React", 'Postgre SQL', "Node.js"],
-    image: "/project1.png",
-    icon: "",
+    image: "./images/ComingSoonBanner.png",
+    icon: "./images/ComingSoon-icon.png",
     githubLink: "https://github.com/TonytheAlias/OtakuSearch",
     feature: true
   }, 
